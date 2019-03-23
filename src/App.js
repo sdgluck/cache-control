71123064760129a7e6cb2f5c09637264d3d5e872
Sam Gluck <sdgluck@gmail.com>
2019-03-23 16:14:28 +0000

    tidy up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import copyToClipboard from "clipboard-copy";
 import SyntaxHighlighter from "react-syntax-highlighter";
 
 import { libraryCode, libraryComment, libraryLanguage } from "./lib-code";
-import { Times, Directives } from "./constants";
+import { Times, Directives, DirectiveDescriptions } from "./constants";
 import { styles, readInDirectives, createHeaderArg } from "./util";
 
 import "normalize.css/normalize.css";
@@ -21,7 +21,13 @@ if (!initialOpenDirectives.length) {
   initialOpenDirectives = directivePriorities.slice(0, 4);
 }
 
-function Fieldset({ title, description, active, fields = () => {} }) {
+function Fieldset({
+  title,
+  api,
+  timeFields = false,
+  enables = [],
+  disables = []
+}) {
   const [open, setOpen] = useState(initialOpenDirectives.includes(title));
 
   return (
@@ -37,8 +43,38 @@ function Fieldset({ title, description, active, fields = () => {} }) {
         <span className="Fieldset__header-open-icon">{open ? "–" : "+"}</span>
       </div>
       <div className="Fieldset__body" style={styles.hide(!open)}>
-        <div className="Fieldset__description">{description}</div>
-        <div className="Fieldset__fields">{fields(active)}</div>
+        <div className="Fieldset__description">
+          <p>{DirectiveDescriptions[title]}</p>
+        </div>
+        <div className="Fieldset__fields">
+          <label key={0}>
+            <input
+              name={title}
+              type="checkbox"
+              checked={api.hasDirective(title)}
+              onChange={() =>
+                api.toggleDirective(
+                  title,
+                  timeFields ? 86400 : null,
+                  disables,
+                  enables
+                )
+              }
+            />
+            {title}
+          </label>
+          {timeFields ? (
+            <TimeInput
+              key={1}
+              active={api.hasDirective(title)}
+              name={title + "_time"}
+              value={api.getDirectiveArg(title) || 0}
+              onArgChange={evt => api.updateDirectiveArg(evt, title)}
+              onTimeChange={evt => api.updateDirectiveTime(evt, title)}
+              time={api.getDirectiveTime(title)}
+            />
+          ) : null}
+        </div>
       </div>
     </div>
   );
@@ -147,7 +183,7 @@ export default class App extends React.Component {
     return !!this.state.directives.find(d => d.name === directive);
   }
 
-  toggleDirective(name, arg = null, disables = []) {
+  toggleDirective(name, arg = null, disables = [], enables = []) {
     const active = this.hasDirective(name);
     let directives = this.state.directives;
     if (active) {
@@ -155,6 +191,13 @@ export default class App extends React.Component {
     } else {
       directives = directives.filter(d => !disables.includes(d.name));
       directives.push({ name, arg, time: Times.secs });
+      if (enables.length) {
+        enables.forEach(d => {
+          if (!this.hasDirective(d.name)) {
+            directives.push(d);
+          }
+        });
+      }
     }
     this.setDirectives(directives);
   }
@@ -291,307 +334,55 @@ export default class App extends React.Component {
           <form action="#" className="Config">
             <Fieldset
               title={Directives.public}
-              description={
-                <p>
-                  Indicates that the response may be cached by any cache, even
-                  if the response would normally be non-cacheable (e.g. if the
-                  response does not contain a max-age directive or the Expires
-                  header).
-                </p>
-              }
-              active={this.hasDirective(Directives.public)}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives.public}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives.public, null, [
-                        Directives.private
-                      ])
-                    }
-                  />
-                  public
-                </label>
-              ]}
+              api={this}
+              disables={[Directives["private"]]}
             />
             <Fieldset
               title={Directives.private}
-              description={
-                <p>
-                  Indicates that the response is intended for a single user and
-                  must not be stored by a shared cache. A private cache may
-                  store the response.
-                </p>
-              }
-              active={this.hasDirective(Directives.private)}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives.public}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives.private, null, [
-                        Directives.public
-                      ])
-                    }
-                  />
-                  private
-                </label>
-              ]}
+              api={this}
+              disables={[Directives["public"]]}
+            />
+            <Fieldset title={Directives["no-cache"]} api={this} />
+            <Fieldset title={Directives["no-store"]} api={this} />
+            <Fieldset title={Directives["max-age"]} api={this} timeFields />
+            <Fieldset title={Directives["s-maxage"]} api={this} timeFields />
+            <Fieldset title={Directives["max-stale"]} api={this} timeFields />
+            <Fieldset title={Directives["min-fresh"]} api={this} timeFields />
+            <Fieldset
+              title={Directives["stale-while-revalidate"]}
+              api={this}
+              timeFields
             />
             <Fieldset
-              title={Directives["max-age"]}
-              description={
-                <p>
-                  Specifies the maximum amount of time a resource will be
-                  considered fresh. Contrary to Expires, this directive is
-                  relative to the time of the request.
-                </p>
-              }
-              active={this.hasDirective(Directives["max-age"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["max-age"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["max-age"], 60 * 60 * 24)
-                    }
-                  />
-                  max-age
-                </label>,
-                <TimeInput
-                  key={1}
-                  active={this.hasDirective(Directives["max-age"])}
-                  name={Directives["max-age"]}
-                  value={this.getDirectiveArg(Directives["max-age"]) || 0}
-                  onArgChange={evt =>
-                    this.updateDirectiveArg(evt, Directives["max-age"])
-                  }
-                  onTimeChange={evt =>
-                    this.updateDirectiveTime(evt, Directives["max-age"])
-                  }
-                  time={this.getDirectiveTime(Directives["max-age"])}
-                />
-              ]}
-            />
-            <Fieldset
-              title={Directives["s-maxage"]}
-              description={
-                <p>
-                  Takes precedence over max-age or the Expires header, but it
-                  only applies to shared caches (e.g., proxies) and is ignored
-                  by a private cache.
-                </p>
-              }
-              active={this.hasDirective(Directives["s-maxage"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["s-maxage"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["s-maxage"], 60 * 60 * 24)
-                    }
-                  />
-                  s-maxage
-                </label>,
-                <TimeInput
-                  key={1}
-                  active={this.hasDirective(Directives["s-maxage"])}
-                  name={Directives["s-maxage"]}
-                  value={this.getDirectiveArg(Directives["s-maxage"]) || 0}
-                  onArgChange={evt =>
-                    this.updateDirectiveArg(evt, Directives["s-maxage"])
-                  }
-                  onTimeChange={evt =>
-                    this.updateDirectiveTime(evt, Directives["s-maxage"])
-                  }
-                  time={this.getDirectiveTime(Directives["s-maxage"])}
-                />
-              ]}
-            />
-            <Fieldset
-              title={Directives["no-store"]}
-              description={
-                <p>
-                  The cache should not store anything about the client request
-                  or server response.
-                </p>
-              }
-              active={this.hasDirective(Directives["no-store"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["no-store"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["no-store"])
-                    }
-                  />
-                  no-store
-                </label>
-              ]}
-            />
-            <Fieldset
-              title={Directives["no-cache"]}
-              description={
-                <p>
-                  Forces caches to submit the request to the origin server for
-                  validation before releasing a cached copy.
-                </p>
-              }
-              active={this.hasDirective(Directives["no-cache"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["no-cache"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["no-cache"])
-                    }
-                  />
-                  no-cache
-                </label>
-              ]}
-            />
-            <Fieldset
-              title={Directives["only-if-cached"]}
-              description={
-                <p>
-                  Indicates to not retrieve new data. This being the case, the
-                  server wishes the client to obtain a response only once and
-                  then cache. From this moment the client should keep releasing
-                  a cached copy and avoid contacting the origin-server to see if
-                  a newer copy exists.
-                </p>
-              }
-              active={this.hasDirective(Directives["only-if-cached"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["only-if-cached"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["only-if-cached"])
-                    }
-                  />
-                  only-if-cached
-                </label>
-              ]}
+              title={Directives["stale-if-error"]}
+              api={this}
+              timeFields
             />
             <Fieldset
               title={Directives["must-revalidate"]}
-              description={
-                <>
-                  <p>
-                    Where no-cache will immediately revalidate with the server,
-                    and only use a cached copy if the server says it may,
-                    must-revalidate is like no-cache with a grace period.
-                  </p>
-                  <p>
-                    must-revalidate needs an associated max-age directive;
-                    above, we’ve set it to ten minutes.
-                  </p>
-                </>
-              }
-              active={this.hasDirective(Directives["must-revalidate"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["must-revalidate"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["must-revalidate"])
-                    }
-                  />
-                  must-revalidate
-                </label>
+              api={this}
+              enables={[
+                {
+                  name: Directives["max-age"],
+                  arg: 60 * 60 * 24,
+                  time: Times.secs
+                }
               ]}
             />
             <Fieldset
               title={Directives["proxy-revalidate"]}
-              description={
-                <p>
-                  In a similar vein to s-maxage, proxy-revalidate is the
-                  public-cache specific version of must-revalidate. It is simply
-                  ignored by private caches.
-                </p>
-              }
-              active={this.hasDirective(Directives["proxy-revalidate"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["proxy-revalidate"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["proxy-revalidate"])
-                    }
-                  />
-                  proxy-revalidate
-                </label>
+              api={this}
+              enables={[
+                {
+                  name: Directives["max-age"],
+                  arg: 60 * 60 * 24,
+                  time: Times.secs
+                }
               ]}
             />
-            <Fieldset
-              title={Directives["immutable"]}
-              description={
-                <p>
-                  immutable is a way of telling the browser that a file will
-                  never change—it’s immutable—and therefore never to bother
-                  revalidating it. We can completely cut out the overhead of a
-                  roundtrip of latency.
-                </p>
-              }
-              active={this.hasDirective(Directives["immutable"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["immutable"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["immutable"])
-                    }
-                  />
-                  immutable
-                </label>
-              ]}
-            />
-            <Fieldset
-              title={Directives["stale-while-revalidate"]}
-              description={
-                <p>
-                  Indicates that the client is willing to accept a stale
-                  response while asynchronously checking in the background for a
-                  fresh one. The seconds value indicates for how long the client
-                  is willing to accept a stale response.
-                </p>
-              }
-              active={this.hasDirective(Directives["stale-while-revalidate"])}
-              fields={active => [
-                <label key={0}>
-                  <input
-                    name={Directives["stale-while-revalidate"]}
-                    type="checkbox"
-                    checked={active}
-                    onChange={() =>
-                      this.toggleDirective(Directives["stale-while-revalidate"])
-                    }
-                  />
-                  stale-while-revalidate
-                </label>
-              ]}
-            />
+            <Fieldset title={Directives["immutable"]} api={this} />
+            <Fieldset title={Directives["no-transform"]} api={this} />
+            <Fieldset title={Directives["only-if-cached"]} api={this} />
           </form>
           <div className="Result" onClick={() => this.copyToClipboard()}>
             <div className="ResultHeader">
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,10 @@
 export const Directives = {
   public: "public",
   private: "private",
+  "no-cache": "no-cache",
+  "no-store": "no-store",
   "max-age": "max-age",
   "s-maxage": "s-maxage",
-  "no-cache": "no-cache",
   "only-if-cached": "only-if-cached",
   "max-stale": "max-stale",
   "min-fresh": "min-fresh",
@@ -12,8 +13,40 @@ export const Directives = {
   "must-revalidate": "must-revalidate",
   "proxy-revalidate": "proxy-revalidate",
   immutable: "immutable",
-  "no-store": "no-store",
   "no-transform": "no-transform"
+};
+
+export const DirectiveDescriptions = {
+  [Directives.public]:
+    "Indicates that the response may be cached by any cache, even if the response would normally be non-cacheable (e.g. if the response does not contain a max-age directive or the Expires header).",
+  [Directives.private]:
+    "Indicates that the response is intended for a single user and must not be stored by a shared cache. A private cache may store the response.",
+  [Directives["max-age"]]:
+    "Specifies the maximum amount of time a resource will be considered fresh. Contrary to Expires, this directive is relative to the time of the request.",
+  [Directives["s-maxage"]]:
+    "Takes precedence over max-age or the Expires header, but it only applies to shared caches (e.g., proxies) and is ignored by a private cache.",
+  [Directives["no-store"]]:
+    "The cache should not store anything about the client request or server response.",
+  [Directives["no-cache"]]:
+    "Forces caches to submit the request to the origin server for validation before releasing a cached copy.",
+  [Directives["only-if-cached"]]:
+    "Indicates to not retrieve new data. This being the case, the server wishes the client to obtain a response only once and then cache. From this moment the client should keep releasing a cached copy and avoid contacting the origin-server to see if a newer copy exists.",
+  [Directives["must-revalidate"]]:
+    "Indicates that once a resource has become stale (e.g. max-age has expired), a cache must not use the response to satisfy subsequent requests for this resource without successful validation on the origin server.",
+  [Directives["proxy-revalidate"]]:
+    "Same as must-revalidate, but it only applies to shared caches (e.g., proxies) and is ignored by a private cache.",
+  [Directives["immutable"]]:
+    "Indicates that the response body will not change over time. The resource, if unexpired, is unchanged on the server and therefore the client should not send a conditional revalidation for it (e.g. If-None-Match or If-Modified-Since) to check for updates, even when the user explicitly refreshes the page.",
+  [Directives["stale-while-revalidate"]]:
+    "Indicates that the client is willing to accept a stale response while asynchronously checking in the background for a fresh one. The seconds value indicates for how long the client is willing to accept a stale response.",
+  [Directives["stale-if-error"]]:
+    "Indicates that the client is willing to accept a stale response if the check for a fresh one fails. The seconds value indicates for how long the client is willing to accept the stale response after the initial expiration.",
+  [Directives["no-transform"]]:
+    "No transformations or conversions should be made to the resource. The Content-Encoding, Content-Range, Content-Type headers must not be modified by a proxy. A non- transparent proxy might, for example, convert between image formats in order to save cache space or to reduce the amount of traffic on a slow link. The no-transform directive disallows this.",
+  [Directives["min-fresh"]]:
+    "Indicates that the client wants a response that will still be fresh for at least the specified number of seconds.",
+  [Directives["max-stale"]]:
+    "Indicates that the client is willing to accept a response that has exceeded its expiration time. Optionally, you can assign a value in seconds, indicating the time the response must not be expired by."
 };
 
 export const Times = {
